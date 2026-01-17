d00ee5f34dd91e341f329066843c274cb9ab4136
bharatayasa40 <bharatayasa40@gmail.com>
2026-01-17 17:46:29 +0800

    deploy to vercel

--- src/property/property-images.interceptor.ts
+++ src/property/property-images.interceptor.ts
@@ -1,26 +1,29 @@
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
-import { extname, join } from 'path';
+import { extname } from 'path';
 import { existsSync, mkdirSync } from 'fs';
 
 export function PropertyImagesInterceptor() {
     return FilesInterceptor('images', 10, {
         storage: diskStorage({
-            destination: (req, file, cb) => {
-                // Gunakan /tmp di Vercel, atau folder lokal di development
-                const uploadPath = process.env.NODE_ENV === 'production'
-                    ? '/tmp'
-                    : './propertyImages';
+            destination: (req: any, file: any, cb: any) => {
+
+                const isVercel = !!process.env.VERCEL;
+
+                const uploadPath = isVercel
+                    ? '/tmp/propertyImages'
+                    : 'propertyImages';
 
                 if (!existsSync(uploadPath)) {
                     mkdirSync(uploadPath, { recursive: true });
                 }
+
                 cb(null, uploadPath);
             },
-            filename: (req: any, file: any, cb: any) => {
+
+            filename: (req, file, cb) => {
                 const randomName = Math.random().toString(36).substring(2, 14);
-                const filename = `property-${randomName}${extname(file.originalname)}`;
-                cb(null, filename);
+                cb(null, `property-${randomName}${extname(file.originalname)}`);
             },
         }),
     });
